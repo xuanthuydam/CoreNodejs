731e7366ffdd87e8acbe7d6f2fe617dc6e448f9e
THUYDAM\84383 <xuanthuydam.06072000@gmail.com>
2024-08-06 23:16:00 +0700

    [Update] CORS 06/08 - 11:15PM

--- src/app.js
+++ src/app.js
@@ -8,12 +8,14 @@ import authRouter from "./routes/auth/authRouter.js";
 import userManagement from "./routes/api_common/listForUserManagementRouter.js";
 import { authenticateJWT } from "./middleware/jwt/authMiddleware.js";
 import handleValidationErrors from "./middleware/validator/validationMiddleware.js";
+import cors from "cors";
 
 const { json, urlencoded } = bodyParser;
 
 config(); // Đọc các biến môi trường từ file .env
 
 const app = express();
+app.use(cors());
 
 app.use(json());
 app.use(urlencoded({ extended: true }));
